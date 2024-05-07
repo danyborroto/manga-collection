eeb12675c21caf9f0b818fdb17da0d1d36bdd6ce
danyborroto <danyborroto@gmail.com>
2024-05-07 17:40:31 -0400

    Add new View

--- src/controller/SceneController.java
+++ src/controller/SceneController.java
@@ -4,10 +4,17 @@
  */
 package controller;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.control.Button;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.VBox;
 
@@ -22,6 +29,16 @@ public class SceneController implements Initializable {
     private VBox leftMenu;
     @FXML
     private AnchorPane rightPane;
+    @FXML
+    private Button mangaBTN;
+    @FXML
+    private Button genereBTN;
+    @FXML
+    private Button authorBTN;
+    @FXML
+    private Button serieBTN;
+    @FXML
+    private Button settingBTN;
 
     /**
      * Initializes the controller class.
@@ -30,5 +47,60 @@ public class SceneController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
-    
+
+    @FXML
+    private void onMangaView(ActionEvent event) {
+        try {
+            changeFXML("/view/Manga.fxml");
+        } catch (IOException ex) {
+            Logger.getLogger(SceneController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    @FXML
+    private void onGenereView(ActionEvent event) {
+        try {
+            changeFXML("/view/Genere.fxml");
+        } catch (IOException ex) {
+            Logger.getLogger(SceneController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    @FXML
+    private void onAuthorView(ActionEvent event) {
+        try {
+            changeFXML("/view/Author.fxml");
+        } catch (IOException ex) {
+            Logger.getLogger(SceneController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    @FXML
+    private void onSerieView(ActionEvent event) {
+        try {
+            changeFXML("/view/Serie.fxml");
+        } catch (IOException ex) {
+            Logger.getLogger(SceneController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    @FXML
+    private void onSettingView(ActionEvent event) {
+        try {
+            changeFXML("/view/Setting.fxml");
+        } catch (IOException ex) {
+            Logger.getLogger(SceneController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+ 
+    private void changeFXML(String fxml) throws IOException{
+        FXMLLoader loader = new FXMLLoader(getClass().getResource(fxml));
+        AnchorPane panel = loader.load();
+        
+        AnchorPane.setBottomAnchor(panel, Double.valueOf(0));
+        AnchorPane.setLeftAnchor(panel, Double.valueOf(0));
+        AnchorPane.setRightAnchor(panel, Double.valueOf(0));
+        AnchorPane.setTopAnchor(panel, Double.valueOf(0));
+        rightPane.getChildren().setAll(panel);
+    }
 }
