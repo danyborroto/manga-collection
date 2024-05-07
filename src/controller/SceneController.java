09cad8086b049c89ccb1b3572ea3d1271a81c811
danyborroto <danyborroto@gmail.com>
2024-05-07 16:58:03 -0400

    Build main scene

---
+++ src/controller/SceneController.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
+
+/**
+ * FXML Controller class
+ *
+ * @author Lily
+ */
+public class SceneController implements Initializable {
+
+    @FXML
+    private VBox leftMenu;
+    @FXML
+    private AnchorPane rightPane;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
